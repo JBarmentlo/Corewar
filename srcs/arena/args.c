d00b335a4fb1cffbbbe583ed7517f78ee602e4e7
Joep BARMENTLO <joepbarmentlo@gmail.com>
2020-01-14 13:03:12 +0100

    yo

--- srcs/arena/args.c
+++ srcs/arena/args.c
@@ -8,9 +8,15 @@ t_args		*new_t_args(void)
 	return (out);
 }
 
+uint16_t		fill_args(t_arena *arena, t_process *process)
+{
+	if (is_opcode_valid(arena, process->PC))
+
+}
+
 byte		is_opcode_valid(t_arena *arena, uint16_t PC)
 {
-	return (arena->memory[PC] <= 16)
+	return (arena->memory[PC] <= 16);
 }
 
 void		read_encoding_byte(t_arena *arena, t_process *process)
