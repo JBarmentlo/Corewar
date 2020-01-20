a6ef7f290f4f365bd23794563eb7228578de7e1b
Deya BERGER <dberger@e2r10p16.42.fr>
2020-01-20 17:53:24 +0100

    verif size header

--- srcs/arena/pars_champ.c
+++ srcs/arena/pars_champ.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 14:40:55 by dberger           #+#    #+#             */
-/*   Updated: 2020/01/20 17:20:43 by dberger          ###   ########.fr       */
+/*   Updated: 2020/01/20 17:52:25 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@ int		ft_stock_file(t_champion *champ)
 {
 	int		ret;
 	char	buf[BUF_TOT + 1];
+	int		min_header;
 
 	ret = 0;
 	ret = read(champ->fd, buf, BUF_TOT);
 	buf[ret] = '\0';
 	champ->size = ret;
+	min_header = sizeof(COREWAR_EXEC_MAGIC);
+	min_header += PROG_NAME_LENGTH + 4 + 4 + COMMENT_LENGTH + 4 + 1;
+	if (champ->size < min_header)
+		return (error("ERROR: wrong file format", NULL));
 	ft_memcpy(champ->file, buf, ret);
 	if ((ret = read(champ->fd, buf, 2)))
 		return (error("ERROR", NULL));
@@ -30,13 +35,15 @@ int		ft_stock_file(t_champion *champ)
 int		magic_number(t_champion *champ)
 {
 	int		i;
+	int		s;
 	int32_t nb;
 	int32_t n;
 
 	i = 0;
 	nb = 0;
 	n = 0;
-	while (i < 4)
+	s = sizeof(COREWAR_EXEC_MAGIC);
+	while (i < s)
 	{
 		if (nb == 0)
 			nb = champ->file[i] & 0xff;
@@ -48,7 +55,8 @@ int		magic_number(t_champion *champ)
 		}
 		i++;
 	}
-	ft_printf("\n");
+	if (nb != COREWAR_EXEC_MAGIC)
+		return (FALSE);
 	ft_printf("numero magique = %d, nb=%d\n", COREWAR_EXEC_MAGIC, nb);
 	return (TRUE);
 }	
