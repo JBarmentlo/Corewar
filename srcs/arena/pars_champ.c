ee436e23a1beba7d4b083319f996f16d86423759
Deya BERGER <dberger@e2r10p16.42.fr>
2020-01-20 19:44:00 +0100

    champ avec magic num, name et comment

--- srcs/arena/pars_champ.c
+++ srcs/arena/pars_champ.c
@@ -6,11 +6,12 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 14:40:55 by dberger           #+#    #+#             */
-/*   Updated: 2020/01/20 17:52:25 by dberger          ###   ########.fr       */
+/*   Updated: 2020/01/20 19:43:37 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/arena.h"
+#include <stdio.h>
 
 int		ft_stock_file(t_champion *champ)
 {
@@ -21,14 +22,14 @@ int		ft_stock_file(t_champion *champ)
 	ret = 0;
 	ret = read(champ->fd, buf, BUF_TOT);
 	buf[ret] = '\0';
-	champ->size = ret;
-	min_header = sizeof(COREWAR_EXEC_MAGIC);
-	min_header += PROG_NAME_LENGTH + 4 + 4 + COMMENT_LENGTH + 4 + 1;
-	if (champ->size < min_header)
-		return (error("ERROR: wrong file format", NULL));
-	ft_memcpy(champ->file, buf, ret);
+	champ->prog_size = ret;
+	min_header = sizeof(COREWAR_EXEC_MAGIC) + PROG_NAME_LENGTH;
+	min_header +=  PADDING + 4 + COMMENT_LENGTH + PADDING + 1;
+	if (champ->prog_size < min_header)
+		return (error("ERROR: file too small", NULL));
+	ft_memcpy(champ->prog, buf, ret);
 	if ((ret = read(champ->fd, buf, 2)))
-		return (error("ERROR", NULL));
+		return (error("ERROR: file too big", NULL));
 	return (TRUE);
 }
 
@@ -36,21 +37,39 @@ int		magic_number(t_champion *champ)
 {
 	int		i;
 	int		s;
-	int32_t nb;
+	uint32_t nb;
 	int32_t n;
 
 	i = 0;
 	nb = 0;
 	n = 0;
 	s = sizeof(COREWAR_EXEC_MAGIC);
+/*	nb =*(uint32_t*)champ->prog;
+	while (i < 4)
+	{
+		printf("[%02hhx] ", ((char*)&nb)[i]);
+		i++;
+	}
+	ft_printf("BEFORE numero magique = %d, nb=%d\n", COREWAR_EXEC_MAGIC, nb);
+	printf("\n");
+	i = 0;
+	n = COREWAR_EXEC_MAGIC;
+	printf("\n");
+	while (i < 4)
+	{
+		printf("[%02hhx] ", ((char*)&n)[i]);
+		i++;
+	}
+	n = 0;
+	i = 0;*/
 	while (i < s)
 	{
 		if (nb == 0)
-			nb = champ->file[i] & 0xff;
+			nb = champ->prog[i] & 0xff;
 		else
 		{
 			nb = nb << 8;
-			n = champ->file[i] & 0xff;
+			n = champ->prog[i] & 0xff;
 			nb = nb | n;
 		}
 		i++;
@@ -59,17 +78,32 @@ int		magic_number(t_champion *champ)
 		return (FALSE);
 	ft_printf("numero magique = %d, nb=%d\n", COREWAR_EXEC_MAGIC, nb);
 	return (TRUE);
-}	
+}
+
+void	champ_name_comment(t_champion *champ)
+{
+	int		i;
+
+	i = sizeof(COREWAR_EXEC_MAGIC);
+	ft_bzero(champ->name, PROG_NAME_LENGTH + PADDING);
+	ft_bzero(champ->comment, COMMENT_LENGTH + PADDING);
+	ft_memcpy(champ->name, champ->prog + i, PROG_NAME_LENGTH + PADDING); 
+	i += PROG_NAME_LENGTH + PADDING + 4;
+	ft_memcpy(champ->comment, champ->prog + i, COMMENT_LENGTH + PADDING); 
+}
 
 int		pars_champ(t_arena *vm, int indx)
 {
 	t_champion	champ;
 
 	champ = vm->champion_table[indx];
-	ft_bzero(champ.file, BUF_TOT);
+	ft_bzero(champ.prog, BUF_TOT);
 	if (ft_stock_file(&champ) == FALSE)
 		return (FALSE);
 	if (magic_number(&champ) == FALSE)
 		return (FALSE); 
+	champ_name_comment(&champ);
+	ft_printf("name = [%s]\n", champ.name);
+	ft_printf("comment = [%s]\n", champ.comment);
 	return (TRUE);
 }
