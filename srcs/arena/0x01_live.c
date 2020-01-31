9423b2d6a541af1920e84d7c49f42df1193e00ef
Joep BARMENTLO <joepbarmentlo@gmail.com>
2020-01-31 19:29:09 +0100

    fixed live

--- srcs/arena/0x01_live.c
+++ srcs/arena/0x01_live.c
@@ -1,17 +1,20 @@
 #include "arena.h"
 
 
-// nb or -nb ?
-// if wrong arg does process-->last_live update ?
+// NEED TO ADD DEAD CHAMPS TO FILL TAB
 void    x01(t_arena *arena, t_process *process)
 {
+	int	arg;
+
     printf("0x01\n");
 	print_t_args(arena->args);
     process->last_live = arena->cycle;
     arena->total_live_since_check++;
-    if (arena->args->val[0] == ((-1) * process->owner->number))
-    {
-		arena->champion_table[(-1) * arena->args->val[0] - 1].lives_since_last_check += 1;
-        arena->last_live_champ_number = (-1) * arena->args->val[0];
-    }
+	arg = (-1) * arena->args->val[0] - 1;
+	if (arg < MAX_PLAYERS && arg > 0 && arena->champion_table[arg].alive)
+	{
+			arena->champion_table[arg].lives_since_last_check += 1;
+   			arena->last_live_champ_number = arg;
+	}
+
 }
