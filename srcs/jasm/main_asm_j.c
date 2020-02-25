c4910b8eb060f032deb2cab74222302abb26e307
Joep BARMENTLO <joepbarmentlo@gmail.com>
2020-02-25 16:59:37 +0100

    bs

--- srcs/jasm/main_asm_j.c
+++ srcs/jasm/main_asm_j.c
@@ -2,12 +2,32 @@
 #include "jasm.h"
 #include "stdio.h"
 #include "unistd.h"
+#include "stdlib.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+typedef struct	s_cor
+{
+	header_t	header;
+
+}				t_cor;
+
+
 
 int	main(int ac, char **av)
 {
-	header_t header;
+	struct stat ss;
+	int			fd;
+	char		*text;
 
 	is_file_ok(av[1]);
+	fd = open(av[1], O_RDONLY);
+	fstat(fd, &ss);
+	printf("size: %lld\n", ss.st_size);
+	text = malloc(ss.st_size + 1);
+	read(fd, text, ss.st_size);
 	ac++;
+	printf("%s", text);
 
 }
