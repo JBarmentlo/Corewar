570690a0b64847da5efe48608ed195ce30c6a5ec
Joep BARMENTLO <joepbarmentlo@gmail.com>
2020-03-05 16:45:38 +0100

    prettier live print

--- srcs/arena/0x01_live.c
+++ srcs/arena/0x01_live.c
@@ -20,6 +20,8 @@ void    x01(t_arena *arena, t_process *process)
 	{
 		printf("live %d\n", arg);
 		printf("last_live_nb : %d\n", arena->last_live_champ_number);
-		printf("total lives: %d\n", arena->champion_table[arg - 1].lives_since_last_check);
+		if (arg <= MAX_PLAYERS && arg >= 0)
+			printf("total lives: %d\n", arena->champion_table[arg - 1].lives_since_last_check);
 	}
+	
 }
