def67cc0b7bd8a1cda73bf7b823c03721056af63
ncoursol <50494077+ncoursol@users.noreply.github.com>
2020-05-01 17:14:59 +0200

    main_visu a la norme

--- srcs/arena/main_visu.c
+++ srcs/arena/main_visu.c
@@ -22,7 +22,7 @@ t_arena		init_vm(void)
 	return (vm);
 }
 
-int			assign_champ(t_arena *vm)
+int		assign_champ(t_arena *vm)
 {
 	int			i;
 	t_champion	*champ;
@@ -40,19 +40,64 @@ int			assign_champ(t_arena *vm)
 	return (TRUE);
 }
 
-int			main(int ac, char **av)
+void		loop_poll(t_disp *d, t_arena *vm, int *running, int timeout)
+{
+	int	i;
+	
+	i = SDL_GetTicks() + 100;
+	while (SDL_PollEvent(&d->event)
+	|| (!SDL_TICKS_PASSED(SDL_GetTicks(), timeout) && *running != 0)
+	|| (d->pause != 0 && d->step != 1))
+	{
+		d->step = 0;
+		events(d, running, &timeout, *vm);
+		if (SDL_TICKS_PASSED(SDL_GetTicks(), i))
+		{
+			if (!(SDL_GetTicks() % d->d_cycle))
+				do_the_cycle(vm);
+			d->step = 0;
+			events(d, running, &timeout, *vm);
+			if (SDL_TICKS_PASSED(SDL_GetTicks(), i))
+			{
+				d->button_status = 0;
+				i = SDL_GetTicks() + 200;
+			}
+		}
+	}
+}
+
+int		loop(t_disp *d, t_arena *vm, int *running)
+{
+	unsigned int	j;
+	int		timeout;
+	
+	if (vm->cycle == UINT64_MAX)
+	{
+		ft_printf("dude this is taking forever\n");
+		return (0);
+	}
+	timeout = SDL_GetTicks() + 100;
+	j = 0;
+	while (j < d->delay && ((vm->cycle < (unsigned long)vm->option_dump)
+	|| vm->option_dump == 0))
+	{
+		do_the_cycle(vm);
+		j++;
+	}
+	loop_poll(d, vm, running, timeout);
+	update_visu(&d, vm);
+	return (1);
+}
+
+int		main(int ac, char **av)
 {
 	t_disp			d;
-	int				timeout;
 	int				running;
 	t_arena			vm;
-	int				i;
-	unsigned int	j;
 
-	i = 0;
 	vm = init_vm();
 	if (pars_args(ac, av, &vm) == FALSE
-		|| assign_champ(&vm) == FALSE)
+	|| assign_champ(&vm) == FALSE)
 		return (FALSE);
 	init_window(&d, vm);
 	running = 1;
@@ -61,40 +106,8 @@ int			main(int ac, char **av)
 	while (!is_game_over(&vm) && running
 		&& ((vm.cycle < (unsigned long)vm.option_dump) || vm.option_dump == 0))
 	{
-		if (vm.cycle == UINT64_MAX)
-		{
-			ft_printf("dude this is taking forever\n");
+		if (!loop(&d, &vm, &running))
 			break ;
-		}
-		timeout = SDL_GetTicks() + 100;
-		i = SDL_GetTicks() + 100;
-		j = 0;
-		while (j < d.delay && ((vm.cycle < (unsigned long)vm.option_dump)
-			|| vm.option_dump == 0))
-		{
-			do_the_cycle(&vm);
-			j++;
-		}
-		while (SDL_PollEvent(&d.event)
-				|| (!SDL_TICKS_PASSED(SDL_GetTicks(), timeout) && running != 0)
-				|| (d.pause != 0 && d.step != 1))
-		{
-			d.step = 0;
-			events(&d, &running, &timeout, vm);
-			if (SDL_TICKS_PASSED(SDL_GetTicks(), i))
-			{
-				if (!(SDL_GetTicks() % d.d_cycle))
-					do_the_cycle(&vm);
-				d.step = 0;
-				events(&d, &running, &timeout, vm);
-				if (SDL_TICKS_PASSED(SDL_GetTicks(), i))
-				{
-					d.button_status = 0;
-					i = SDL_GetTicks() + 200;
-				}
-			}
-		}
-		update_visu(&d, vm);
 	}
 	if (vm.option_dump != 0)
 		hex_dump_ugly(&vm);
@@ -102,10 +115,3 @@ int			main(int ac, char **av)
 	free_all(&vm);
 	return (TRUE);
 }
-
-/*
-**__attribute__((destructor)) void test()
-**{
-**	while(1);
-**}
-*/
