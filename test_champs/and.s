eebdafcfef9695a4f0caf0b824e5232bd2b4b18a
Yoopster <joepbarmentlo@gmail.com>
2020-05-06 15:53:14 +0200

    testing

--- test_champs/and.s
+++ test_champs/and.s
@@ -2,6 +2,13 @@
 .comment "no comment"
 
 and %31,%15,r2
-and %7,r2,r3
-st r2,20
-and %7,15,r4
+and %07,r2,r3
+st r2,200
+and %7,195,r4
+st r2,205
+and 200,r2,r2
+and 194,%15,r2
+and 185,185,r2
+and r2,178,r2
+and r2,%15,r2
+and r2,r2,r2
---
+++ test_champs/zjump.s
@@ -0,0 +1,6 @@
+.name "zjump tester"
+.comment "no comment"
+
+zjmp %40
+add r2,r2,r2
+zjmp %40
